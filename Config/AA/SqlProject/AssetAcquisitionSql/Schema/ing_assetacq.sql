c50099e60bd424fa9da04add18d3061d1f3763e2
Kelvin Hoover <kelvin.hoover@hotmail.com>
2023-02-14 11:46:58 -0800

    Remove V1

--- Config/AA/SqlProject/AssetAcquisitionSql/Schema/ing_assetacq.sql
+++
@@ -1,1 +0,0 @@
-CREATE SCHEMA ing_assetacq;
--- Config/AA/SqlProject/AssetAcquisitionSql/clt_AssetAcq/Tables/InvestorImport.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [clt_AssetAcq].[InvestorImport]
-(
-	[RowId]           uniqueidentifier not null,
-	[MonthEndDate]    bigint not null,
-	[MemberNumber]    [nvarchar](100) not null,
-	[OldMemberNumber] [nvarchar](100) not null,
-	[LoanNumber]      [nvarchar](100) not null,
-	[LoanId]          [nvarchar](100) not null
-)
